674300782de60fba1129c0e353b47ee5857b8b16
Isaac McAuley <mcauleyisaac@gmail.com>
2018-10-24 16:24:30 -0600

    Changed the stats to a numpy matrix to do matrix operations

--- src/Player.py
+++ src/Player.py
@@ -1,8 +1,27 @@
+import numpy as np
+
 class Player:
+    # Important Stats:
+    # G
+    # MP
+    # PER
+    # TS%
+    # 3PAr
+    # FTr
+    # ORB%
+    # DRB%
+    # AST%
+    # STL%
+    # BLK%
+    # WS
+    # VORP
+    # eFG%
+
+
     def __init__(self, name, pos):
         self.name = name
         self.pos = pos
-        self.stats = {}
+        self.stats = np.empty([14,1])
 
     def add_year(self, year, stats):
         self.stats[year] = stats
